bb4b4d5012388c5f4694b5452c15ae37ba8cd98b
Musaib03 <wanimusaib520@gmail.com>
2024-11-01 16:28:30 +0530

    meteor shower made effecive

--- src/Components/MeteorShowerBackground.js
+++ src/Components/MeteorShowerBackground.js
@@ -1,60 +1,23 @@
 import React from 'react';
-import '../App.css'; // Import your app.css for styles
+import '../App.css';
 
-const starShowerBackground = ({ children }) => {
+const StarShowerBackground = ({ children }) => {
     return (
         <div className="meteor-shower-background">
             {children}
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            <div className="star"></div>
-            {/* Add more stars as needed */}
+            {/* Create 100 stars with unique keys */}
+            {Array.from({ length: 100 }).map((_, index) => (
+                <div className="star" key={index} style={{
+                    '--duration': `${Math.random() * 2 + 1}s`,
+                    '--delay': `${Math.random() * 5}s`,
+                    '--opacity': Math.random() * 0.6 + 0.3,
+                    '--size': `${Math.random() * 4 + 2}px`,
+                    '--left-start': `${Math.random() * 50}vw`,
+                    '--top-start': `${Math.random() * -100}vh`,
+                }}></div>
+            ))}
         </div>
     );
 };
 
-export default starShowerBackground;
+export default StarShowerBackground;
